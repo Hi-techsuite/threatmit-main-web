82348f15458cecc9d1bfb2a4bfe1b5166c342b95
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-07-07 16:07:43 +0100

    added whatsapp link

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import PublicNav from "../components/Nav/PublicNav";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { Offices } from "../config/offices";
 
 const Root = () => {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-x-hidden relative">
+      <div className="flex flex-col  bottom-32 right-8 z-40  fixed">
+        <a
+          className="flex flex-col items-center"
+          target="_blank"
+          href={`https://wa.me/${Offices[0].telephone}`}
+        >
+          <IoLogoWhatsapp className=" text-green-600" size={"50px"} />
+          <span>Chat us Now!!</span>
+        </a>
+      </div>
       <PublicNav />
 
       <Outlet />
