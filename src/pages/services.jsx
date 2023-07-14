d3a23b1a78240d347f11ac500a8147284d9e89f6
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-07-14 12:06:28 +0100

    services

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import OurServices from "../components/OurServices";
 
 const Services = () => {
-  return <div className="min-h-[100vh]"></div>;
+  return (
+    <div className="min-h-[100vh]">
+      <OurServices />
+    </div>
+  );
 };
 
 export default Services;
