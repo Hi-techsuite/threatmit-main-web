ad71628cdab18aa7d3368b4c56cd5d377fe748df
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-07-20 01:32:42 +0100

    added auto play feature for slider

--- src/components/carousels/SlickCarousel.jsx
+++ src/components/carousels/SlickCarousel.jsx
@@ -19,6 +19,11 @@ const SlickCarousel = () => {
     <div>
       <Swiper
         // spaceBetween={50}
+
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         slidesPerView={1}
         modules={[Navigation, Pagination, A11y, Scrollbar, EffectCube]}
         // navigation
@@ -33,16 +38,19 @@ const SlickCarousel = () => {
               // data-aos="zoom-in"
               className="min-h-[70vh] md:min-h-[100vh] relative flex flex-col gap-3 items-center justify-center  w-full h-full  content-[''] top-0 left-0  bg-gradient-to-r from-[#330d70]/30  to-[#f48005]/10   "
             >
-              <div className=" flex  gap-3  flex-col md:bottom-50  md:left-10  md:justify-start justify-center items-center   absolute">
-                <p className="text-2xl text-gray-300 font-light ">
+              <div className=" flex  gap-3  flex-col md:bottom-50  md:left-10  md:justify-start justify-center items-start   absolute">
+                <p className=" text-gray-200 font-light text-2xl md:text-5xl  ">
                   we deliver on
                 </p>
                 <p
                   // data-aos="fade-right"
                   className="font-light text-white  md:text-5xl flex flex-col  text-2xl md:text-start text-center"
                 >
-                  <span className="font-bold">Assets </span> and critical
-                  infrastructure <span className="font-black"> protection</span>
+                  <span className="font-bold">Assets </span> and{" "}
+                  <span className="font-black">
+                    {" "}
+                    Critical infrastructure protection
+                  </span>
                 </p>
 
                 <Link
@@ -61,10 +69,8 @@ const SlickCarousel = () => {
               // data-aos="zoom-in"
               className="min-h-[70vh] md:min-h-[100vh] relative flex flex-col gap-3 items-center justify-center  w-full h-full  content-[''] top-0 left-0  bg-gradient-to-r from-[#330d70]/30  to-[#f48005]/10   "
             >
-              <div className=" flex  gap-3  flex-col      justify-start   absolute">
-                <p className="text-4xl text-gray-300 font-extralight">
-                  with our
-                </p>
+              <div className=" flex  gap-3  flex-col      justify-start  px-3   absolute">
+                <p className="text-4xl text-gray-300 font-semibold">with our</p>
                 <p
                   // data-aos="fade-right"
                   className="font-medium text-white  md:text-6xl flex flex-col  text-4xl w-2/4  "
@@ -81,19 +87,23 @@ const SlickCarousel = () => {
               // data-aos="zoom-in"
               className="min-h-[70vh] md:min-h-[100vh] relative   bg-gradient-to-r from-[#330d70]/50  to-[#f48005]/30   "
             >
-              <div
-                className="   items-center   bottom-10 right-10  absolute w-2/5 px-3 py-5
-               "
-              >
-                <p className="text-2xl  font-extralight bg-black text-white px-2 w-fit">
-                  ACCESS CONTROL SYSTEM
-                </p>
+              <div className=" flex  gap-3  flex-col left-20 top-48  md:justify-start justify-center items-start   absolute">
                 <p
                   // data-aos="fade-right"
-                  className=" text-white font-light  md:text-3xl flex flex-col    "
+                  className="font-light text-white  md:text-5xl flex flex-col  text-2xl md:text-start text-center"
                 >
-                  You can trust us with your ACCESS CONTROL SYSTEM
+                  You can trust us with your{" "}
+                  <span className="font-bold text-4xl">
+                    ACCESS CONTROL SYSTEM
+                  </span>
                 </p>
+                {/* 
+                <Link
+                  to={"/online"}
+                  className="btn normal md:w-2/4  bg-[#F48006]  text-white border-none font-bold  rounded-sm flex  flex-row gap-3"
+                >
+                  more information
+                </Link> */}
               </div>
             </section>
           </div>
@@ -104,19 +114,23 @@ const SlickCarousel = () => {
               // data-aos="zoom-in"
               className="min-h-[70vh] md:min-h-[100vh] relative   bg-gradient-to-l from-[#330d70]/30  to-[#f48005]/10   "
             >
-              <div
-                className="   items-center   bottom-10 right-10  absolute w-2/5 px-3 py-5
-               "
-              >
-                <p className="text-2xl  font-extralight  text-white px-2 w-fit">
-                  Car Repair
-                </p>
+              <div className=" flex  gap-3  flex-col left-20 top-48  md:justify-start justify-center items-start   absolute">
                 <p
                   // data-aos="fade-right"
-                  className=" text-white font-light  md:text-3xl flex flex-col    "
+                  className="font-light text-white  md:text-4xl flex flex-col  text-2xl md:text-start text-center"
                 >
-                  {/* You can trust us with your ACCESS CONTROL SYSTEM */}
+                  We guarantee excellent delivery on{" "}
+                  <span className="font-bold text-4xl text-white">
+                    Auto Mobile Repairs/Maintenance
+                  </span>
                 </p>
+                {/* 
+                <Link
+                  to={"/online"}
+                  className="btn normal md:w-2/4  bg-[#F48006]  text-white border-none font-bold  rounded-sm flex  flex-row gap-3"
+                >
+                  more information
+                </Link> */}
               </div>
             </section>
           </div>
@@ -131,8 +145,8 @@ const SlickCarousel = () => {
                 className="    items-center  transform  translate-x-[-50%] translate-y-[-50%] top-[50%]  left-[50%]  absolute  px-3 py-5
                "
               >
-                <p className="text-2xl  font-bold  text-white px-2 w-fit">
-                  MARITIME SECURITY
+                <p className="text-6xl  font-bold uppercase  text-opacity-90 text-gray-200 px-2  text-center">
+                  Maritime/Offshore Security
                 </p>
                 <p
                   // data-aos="fade-right"
