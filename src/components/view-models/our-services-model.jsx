4f1ad2f1f0202fbeddcbea7ceab5a8d7f121d16f
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-07-28 02:47:25 +0100

    animations

--- src/components/view-models/our-services-model.jsx
+++ src/components/view-models/our-services-model.jsx
@@ -12,7 +12,7 @@ const OurServicesModel = ({ image, title, description, href, position }) => {
   }
   return (
     <div
-      className={`flex  relative flex-col gap-4  my-5 md:px-0 px-7 h-[400px] w-full  group  bg-[url(${image})] `}
+      className={`flex   relative flex-col gap-4  my-5 md:px-0 px-7 h-[400px] w-full bg-black/50 group  bg-blend-saturation hover:bg-blend-normal bg-[url(${image})] scale-95 md:hover:scale-105 duration-300  hover:mx-0 hover:rounded-md hover:z-50 transition-all`}
       style={{
         backgroundImage: `url(${image})`,
         backgroundRepeat: "no-repeat",
@@ -25,11 +25,14 @@ const OurServicesModel = ({ image, title, description, href, position }) => {
         className={` md:object-cover  md:h-full h-[100vh] w-[100vw] hidden  `}
       /> */}
 
-      <div className=" group-hover:hidden absolute bottom-10 left-10">
-        <p className="font-bold text-white">{title}</p>
+      <div className="  absolute bottom-1 left-0 px-2 flex flex-col  justify-start w-full   group-hover:bg-[#330d70]/40  py-3">
+        <p className=" group-hover:font-bold ease-in-out transition-all text-white">
+          {title}
+        </p>
+        <p className="font-light text-white ">{description}</p>
       </div>
 
-      <div className=" backdrop-blur-sm w-full h-full bg-opacity-50 bg-[#330d70] absolute hidden group-hover:flex  top-0 bottom-0 left-0 flex-col  gap-3 py-5 px-3 mx-0  justify-center items-center">
+      <div className=" backdrop-blur-sm w-full h-full bg-opacity-50 bg-[#330d70] absolute hidden   top-0 bottom-0 left-0 flex-col  gap-3 py-5 px-3 mx-0  justify-center items-center">
         <p className="font-black text-white text-center">{title}</p>
         <p className="font-light text-white px-4 text-center">{description}</p>
 
