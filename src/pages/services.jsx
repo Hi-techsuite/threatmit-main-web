312bec7bc8264924860da96ea4f0c70a087579ff
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-08-01 16:33:48 +0100

    update

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -6,23 +6,7 @@ import OurServicesModel from "../components/view-models/our-services-model";
 const Services = () => {
   return (
     <div className="min-h-[100vh]">
-      {/* <OurServices /> */}
-      <div className=" grid-cols-3 gap-3  md:grid-cols-4 md:grid     px-3">
-        {OurServicesData && OurServicesData.length > 1 ? (
-          OurServicesData.map((service, index) => {
-            return (
-              <OurServicesModel
-                image={service.image}
-                title={service.title}
-                description={service.details}
-                position={service.position}
-              />
-            );
-          })
-        ) : (
-          <p>hello</p>
-        )}
-      </div>
+      <OurServices />
     </div>
   );
 };
