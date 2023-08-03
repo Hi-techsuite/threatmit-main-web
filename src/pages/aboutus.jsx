5536395dcfaf0815f34e477e81244787c628f962
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-08-03 14:39:51 +0100

    update

--- src/pages/aboutus.jsx
+++ src/pages/aboutus.jsx
@@ -7,17 +7,16 @@ import Gallery from "../components/Gallery";
 import Testimonies from "../components/Testimonies";
 import { globe_img, padlock_img } from "../config/imageFactory";
 import OurClients from "../components/OurClients";
+import pattern from "../assets/images/pattern3.jpeg";
 
 const AboutUs = () => {
   return (
     <div className="">
       <div
-        className="flex flex-col gap-5 justify-center items-center bg-center  min-h-[50vh]   bg-gray-100/20 text-black bg-no-repeat bg-blend-overlay bg-cover  bg-[url('./src/assets/images/pattern3.jpeg')]  "
-        // style={{
-        //   // height: "200px",
-        //   backgroundImage: ` linear-gradient(to bottom, rgba(97,165,250, 0.42), rgba(0,0,0, 0.73)),
-        //   url(${padlock_img}) ,url(${globe_img})`,
-        // }}
+        className="flex flex-col gap-5 justify-center items-center bg-center  min-h-[50vh]   bg-gray-100/20 text-black bg-no-repeat bg-blend-overlay bg-cover  bg-[url('https://threatmits.com/assets/pattern2.jpeg')]  "
+        style={{
+          backgroundImage: `url(${pattern})`,
+        }}
       >
         <p className="text-center  mt-16 text-3xl text-black font-semibold">
           About us
