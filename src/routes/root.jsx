dcb79b9a34629cd73e18b6b1b34e286dca4fb400
Goodluck Kingsley <goodluckcanhelp@gmail.com>
2023-08-03 13:04:05 +0100

    update

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import PublicNav from "../components/Nav/PublicNav";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
-import { IoLogoWhatsapp } from "react-icons/io";
+import { IoLogoLinkedin, IoLogoWhatsapp } from "react-icons/io";
 import { Offices } from "../config/offices";
 
 const Root = () => {
   return (
     <div className="overflow-x-hidden relative">
-      <div className="flex flex-col  bottom-32 right-8 z-40  fixed">
+      <div className="flex flex-col  gap-3 bottom-32 right-8 z-50  fixed">
+        <a
+          className="flex flex-col items-center"
+          target="_blank"
+          href={`https://www.linkedin.com/company/threatmit-security-and-safety-consultants/?originalSubdomain=ng`}
+        >
+          {/* <IoLogoLinkedin className=" " size={"50px"} /> */}
+          <IoLogoLinkedin className=" " size={"50px"} />
+          {/* <span>Chat us Now!!</span> */}
+        </a>
         <a
           className="flex flex-col items-center"
           target="_blank"
           href={`https://wa.me/${Offices[0].telephone}`}
         >
+          {/* <IoLogoLinkedin className=" " size={"50px"} /> */}
           <IoLogoWhatsapp className=" text-green-600" size={"50px"} />
           {/* <span>Chat us Now!!</span> */}
         </a>
